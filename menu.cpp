9be0bb1d6f17802c37d52c12db5a1edcc6a31a8e
AkhmadSaputraPLBN <c030324006@mahasiswa.poliban.ac.id>
2025-05-12 15:45:07 +0700

    Ujian Tengah Semester

--- menu.cpp
+++ menu.cpp
@@ -2,19 +2,99 @@
 #include <thread>
 #include <conio.h>
 using namespace std;
+int nilai[5];
+int n = sizeof(nilai)/sizeof(nilai[0]);
 
 void info(){
   system("cls");
-    cout << "HIDUP JOKOWI" << endl;
+    cout << "Nama\t: Akhmad Saputra" << endl;
+    cout << "NIM\t: C030324006" << endl;
+    cout << "Kelas\t: TI-2B" << endl;
   getch();
+}
+
+void isi_data(){
+  system("cls");
+  cout << "SILAHKAN ISI DATA" << endl << endl;
+  for (int i=0; i<n; i++){
+    cout << "Masukan Nilai Nomor "<< i+1 << ":";
+    cin >> nilai[i];
+  }
+  getch();
+}
+
+void menampilkan(){
+  system("cls");
+  cout << "MENAMPILKAN DATA" << endl << endl;
+  cout << "Nilai: ";
+  for (int i=0; i<n; i++){
+    cout << nilai[i] << " ";
+  }
+  getch();
+}
+
+void tukar(int *a, int *b){
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void exchange_sort(int nilai[]){
+  system("cls");
+  cout << "EXCHANGE SORTING" << endl;
+  cout << "Tekan huruf A untuk Ascending dan D untuk discending: ";
+  char x = getch();
+  switch(x){
+    case 'a':{
+      system("cls");
+      for (int i=0; i<n-1;i++){
+        for (int j=i+1; j<n; j++){
+          if (nilai[i] > nilai[j]){
+            tukar(&nilai[i], &nilai [j]);
+          }
+          for(int k=0; k<n; k++){
+            cout << nilai[k] << " ";
+          }
+        cout << endl;
+      }
+      cout << endl;
+    }
+    cout << "ANDA BERHASIL ASCENDING" << endl;
+  }
+  break;
+
+  case 'd':{
+    system("cls");
+    for (int i=0; i<n-1;i++){
+      for(int j=i+1; j<n; j++){
+        if(nilai[i] < nilai[j]){
+          tukar(&nilai[i], &nilai [j]);
+        }
+        for(int k=0; k<n; k++){
+          cout << nilai[k] << " ";
+        }
+        cout << endl;
+      }
+      cout << endl;
+    }
+  }
+  cout << endl;
+  cout << "ANDA BERHASIL DISCENDING" << endl;
+  break;
+  default:
+  system("cls");
+    cout << "Input tidak sesuai";
+    break;
+  }
+getch();
 }
 
 void dMenu(){
 system("cls");
 cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
+cout<<"1. Isi Data"<<"\n";            
+cout<<"2. Menampilkan Data"<<"\n";            
+cout<<"3. Exchange Shorting"<<"\n";           
 cout<<"4. Informasi"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
@@ -38,14 +118,14 @@ do
   {
    case '1':
     /* code */
-    mPertama("pertama");
+    isi_data();
     break;
    case '2':
-    mPertama("ke- dua");
+    menampilkan();
     /* code */ 
     break;  
    case '3':
-    mPertama("ke- tiga");
+    exchange_sort(nilai);
     /* code */
     break;  
    case '4':
